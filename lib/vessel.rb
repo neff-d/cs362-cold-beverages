8debaffa7bf28b7267fd04669f7fa03d5810acf9
Daniel Neff <neffd@oregonstate.edu>
2023-03-21 16:57:05 -0700

    steps 1 and 2

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,21 @@
 class Vessel
   attr_reader :volume
+  attr_reader :name
+  attr_accessor :current_volume
+  attr_accessor :initial_fill
 
   def initialize(name, volume)
     @name = name
     @volume = volume
+    @initial_fill = 0
+    @current_volume = initial_fill
   end
 
+  def empty?
+    current_volume == 0
+  end
+
+  def fill
+    @current_volume = volume
+  end
 end
